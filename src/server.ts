73db135bd8dccb7daba8fd85001dfd0bf2b80fc6
joaquinns <joaquindevv96@gmail.com>
2023-01-10 22:55:58 -0400

    server config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
+import mongoose from 'mongoose'
 import { app } from './app'
-import { loggerError, loggerInfo } from './utils/logger'
+import { loggerError, loggerInfo, loggerSuccess } from './utils/logger'
 
-export const server = app.listen(app.get('port'), () => {
+const DB_URI: string = <string>process.env.DB_URI
+
+mongoose
+  .connect(DB_URI)
+  .then((_db) => {
+    loggerInfo('[DB] CONNECTION UP')
+    loggerSuccess('[DB] SUCCESS!')
+  })
+  .catch((e) => {
+    loggerError(`[SERVER ERROR]: ${e}`)
+  })
+
+export const server = app.listen(app.get<number>('port'), () => {
   loggerInfo(`[SERVER RUNNING] listening on port ${app.get('port')}`)
-})
-
-server.on('error', (error) => {
-  loggerError(`[SERVER ERROR] ${error}`)
-  process.exit(1)
 })
