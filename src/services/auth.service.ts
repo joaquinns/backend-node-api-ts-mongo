37292d1974bad985af49a8436ebf015878a0b8de
joaquinns <joaquindevv96@gmail.com>
2023-01-12 14:20:38 -0400

    created auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import { userModel } from '../models/user.model'
+import { authLoginInput } from '../schemas/auth.schema'
+import { loggerError, loggerInfo } from '../utils/logger'
+
+export const authLogin = async (auth: authLoginInput, next: NextFunction) => {
+  try {
+    const foundedUser = await userModel.findOne({ email: auth.email })
+
+    if (!foundedUser) {
+      throw {
+        statusCode: 403,
+        message: 'email or password incorrect'
+      }
+    }
+
+    const validPassword = await foundedUser.comparePasswords(auth.password)
+    if (!validPassword) {
+      loggerInfo('No paso huele bicho')
+      throw {
+        statusCode: 403,
+        message: 'email or password incorrect'
+      }
+    }
+
+    const token = await jwt.sign(
+      {
+        id: foundedUser.id
+      },
+      'secreto',
+      {
+        expiresIn: 86400
+      }
+    )
+
+    return { token }
+  } catch (error: any) {
+    loggerError(
+      `[ERROR AUTH LOGIN SERVICE] ${
+        (error.message, error?.stack && error.stack)
+      }`
+    )
+    next(error)
+  }
+}
