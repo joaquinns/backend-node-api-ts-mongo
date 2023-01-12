7723e02e0a63d55599ced43956591fae3a325c08
joaquinns <joaquindevv96@gmail.com>
2023-01-12 14:34:56 -0400

    added void

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,15 @@
-export const loggerInfo = (msg: string) => {
+export const loggerInfo = (msg: string): void => {
   console.log(`[INFO]: ${msg}`)
 }
 
-export const loggerSuccess = (msg: string) => {
+export const loggerSuccess = (msg: string): void => {
   console.log(`[SUCCESS]: ${msg}`)
 }
 
-export const loggerError = (msg: string) => {
+export const loggerError = (msg: string): void => {
   console.log(`[ERROR]: ${msg}`)
 }
 
-export const loggerWarning = (msg: string) => {
+export const loggerWarning = (msg: string): void => {
   console.log(`[WARNING]: ${msg}`)
 }
