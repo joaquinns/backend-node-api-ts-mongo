a4b72d37294cf651644bdfad754b5bdcf4d6ea4a
joaquinns <joaquindevv96@gmail.com>
2023-01-12 14:22:06 -0400

    added env for test db && function to connect mongo

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,31 @@
+import { createHttpTerminator } from 'http-terminator'
 import mongoose from 'mongoose'
 import { app } from './app'
 import { loggerError, loggerInfo, loggerSuccess } from './utils/logger'
 
-const DB_URI: string = <string>process.env.DB_URI
+const DB_URI: string =
+  process.env.NODE_ENV === 'test'
+    ? <string>process.env.DB_URI_TEST
+    : <string>process.env.DB_URI
 
-mongoose
-  .connect(DB_URI)
-  .then((_db) => {
+mongoose.set('strictQuery', true)
+
+async function db(): Promise<void> {
+  try {
+    await mongoose.connect(DB_URI)
     loggerInfo('[DB] CONNECTION UP')
     loggerSuccess('[DB] SUCCESS!')
-  })
-  .catch((e) => {
-    loggerError(`[SERVER ERROR]: ${e}`)
-  })
+  } catch (error) {
+    loggerError(`[SERVER ERROR]: ${error}`)
+  }
+}
+
+db()
 
 export const server = app.listen(app.get<number>('port'), () => {
   loggerInfo(`[SERVER RUNNING] listening on port ${app.get('port')}`)
+})
+
+export const serverTerminator = createHttpTerminator({
+  server
 })
