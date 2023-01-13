1906f77dac20a293d7280a392f4b3c55463e3c3b
joaquinns <joaquindevv96@gmail.com>
2023-01-13 12:50:29 -0400

    added middleware to profile

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import express, { Express } from 'express'
+import checkToken from '../middleware/checkToken.middleware'
 import authRoutes from './auth.router'
+import profileRoutes from './profile.router'
 import userRoutes from './user.router'
 
 let server: Express = express()
 
-let rootRouter = express.Router()
-
-server.use('/', rootRouter) // api/
 server.use('/users', userRoutes) // api/users
 server.use('/auth', authRoutes) // api/auth
+server.use('/profile', [checkToken], profileRoutes) // api/profile
 
 export default server
